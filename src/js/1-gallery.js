6caf971f8c938dc2ce616a0a905aa2bdca60154d
VyshnevskaO <vyshnevskaolenka@gmail.com>
2024-03-26 23:05:55 +0100

    Added the gallery

---
+++ src/js/1-gallery.js
@@ -0,0 +1,9 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
